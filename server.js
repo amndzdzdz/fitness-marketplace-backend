52c2c0547e812ac898c445d7d8f15996c31190bc
amndzdzdz <zorrofx@gmx.net>
2025-10-01 20:37:39 +0200

    added initial server.js script

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const dotevn = require("dotenv").config();
+
+const port = process.env.PORT;
+const app = express();
+
+app.listen(port, () => {
+  console.log("Server listening on port: ", port);
+});
