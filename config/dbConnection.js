8cebdf9d89e242073d4d4ac27a4bb911e2b17a58
amndzdzdz <zorrofx@gmx.net>
2025-10-02 11:40:55 +0200

    added dbConnection file to connect to my local mongodb database using mongoose

---
+++ config/dbConnection.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+mongoose.Promise = global.Promise;
+const connect = mongoose.connection;
+mongoose.set("strictQuery", true);
+
+const connectDB = async () => {
+  const url = process.env.MONGO_URL;
+
+  connect.on("connected", async () => {
+    console.log("Database connection established!");
+  });
+
+  connect.on("reconnected", async () => {
+    console.log("Database connection reestablished!");
+  });
+
+  connect.on("disconnected", () => {
+    console.log("Database connection disconnected!");
+    console.log("Trying to reconnected to database...");
+
+    setTimeout(() => {
+      mongoose.connect(url, {
+        useNewurlParser: true,
+        useUnifiedTopology: true,
+        keepAlive: true,
+        socketTimoutMS: 3000,
+        connectTimeoutMS: 3000,
+      });
+    }, 3000);
+  });
+
+  connect.on("close", () => {
+    console.log("Database connection closed!");
+  });
+
+  connect.on("error", (error) => {
+    console.log("Databse connection error: " + error);
+  });
+
+  await mongoose
+    .connect(url, {
+      useNewurlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/dbConnection");
 const dotevn = require("dotenv").config();
 
 const port = process.env.PORT;
 const app = express();
+connectDB();
 
 app.use("/api/users", require("./routes/userRoutes"));
 
