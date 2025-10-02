4e1acd1ef6faa1672f4be48b35f08f107654748e
amndzdzdz <zorrofx@gmx.net>
2025-10-02 11:17:44 +0200

    updated server.js to include the usersRoute

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotevn = require("dotenv").config();
 const port = process.env.PORT;
 const app = express();
 
+app.use("/api/users", require("./routes/userRoutes"));
+
 app.listen(port, () => {
   console.log("Server listening on port: ", port);
 });
