eb8a2ea71ea9a794f0059c452f436bc0e6281847
amndzdzdz <zorrofx@gmx.net>
2025-10-02 18:09:39 +0200

    added json parse middleware and errorhandler to server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const connectDB = require("./config/dbConnection");
-const dotevn = require("dotenv").config();
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
 const port = process.env.PORT;
 const app = express();
 connectDB();
 
+app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("Server listening on port: ", port);
