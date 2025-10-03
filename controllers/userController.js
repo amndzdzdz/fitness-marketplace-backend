d19da6697d4d728fe0fec0b1cc30ee059deff770
amndzdzdz <zorrofx@gmx.net>
2025-10-03 08:02:59 +0200

    completed registerUser controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Login the user
 //@route POST /api/users/login
@@ -11,7 +13,31 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/register
 //@access public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User register" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required!");
+  }
+  const availableUser = await User.findOne({ email });
+  if (availableUser) {
+    res.status(400);
+    throw new Error("Email address already exists");
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const createdUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  if (createdUser) {
+    res.status(201).json({
+      _id: createdUser.id,
+      email: createdUser.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("No user was created");
+  }
 });
 
 //@desc Get the current user info
