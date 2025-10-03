3eed6390bca1186c8e2fa5fc97c87390273bed36
amndzdzdz <zorrofx@gmx.net>
2025-10-03 07:33:47 +0200

    added endpoint controller descriptions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,36 @@
 const asyncHandler = require("express-async-handler");
 
+//@desc Login the user
+//@route POST /api/users/login
+//@access public
 const loginUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User login" });
 });
 
+//@desc Register the user
+//@route POST /api/users/register
+//@access public
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User register" });
 });
 
+//@desc Get the current user info
+//@route GET /api/users/current
+//@access public
 const getCurrentUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Current user" });
 });
 
+//@desc Update the user info
+//@route PUT /api/users/current
+//@access public
 const updateUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Updated user" });
 });
 
+//@desc Delete a user
+//@route DELETE /api/users/login
+//@access public
 const deleteUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "deleted user" });
 });
