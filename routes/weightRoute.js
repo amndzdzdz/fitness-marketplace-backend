c12778e2658ddbee3aa00fb352052b47dd508a19
amndzdzdz <zorrofx@gmx.net>
2025-10-04 13:47:29 +0200

    imported missing controllers

--- routes/weightRoute.js
+++ routes/weightRoute.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const validateToken = require("../middleware/validateTokenHandler");
+const {
+  getWeight,
+  setWeight,
+  updateWeight,
+  deleteWeight,
+} = require("../controllers/weightController");
 
 const router = express.Router();
 
 router
   .get("/current", validateToken, getWeight)
-  .put("/current", validateToken, setWeight)
+  .post("/current", validateToken, setWeight)
   .put("/current", validateToken, updateWeight)
   .delete("/current", validateToken, deleteWeight);
 
