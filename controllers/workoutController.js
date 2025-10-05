5ddfe935e520df8160af49f1ac9e9cb09ff1b6ce
amndzdzdz <zorrofx@gmx.net>
2025-10-05 19:23:26 +0200

    added initial workoutController

---
+++ controllers/workoutController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
+//@desc create a workout
+//@route POST /api/workout
+//@acces public
+const createWorkout = "";
+
+//@desc get a workout
+//@route GET /api/workout/
+//@acces public
+const getWorkout = "";
+
+//@desc update a workout
+//@route PUT /api/workout
+//@acces public
+const updateWorkout = "";
+
+//@desc delete a workout
+//@route DELETE /api/workout
+//@acces public
+const deleteWorkout = "";
+
+module.exports = {
+  createWorkout,
+  getWorkout,
+  updateWorkout,
+  deleteWorkout,
+};
