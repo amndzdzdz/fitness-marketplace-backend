df2f8428d48e34609ce317675b3b172a9a28b24a
amndzdzdz <zorrofx@gmx.net>
2025-10-06 20:58:25 +0200

    added createWorkout controller

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,9 +1,24 @@
 const asyncHandler = require("express-async-handler");
+const Workout = require("../models/workoutModel");
 
 //@desc create a workout
 //@route POST /api/workout
-//@acces public
-const createWorkout = "";
+//@acces private
+const createWorkout = asyncHandler(async (req, res) => {
+  const { name, exercises } = req.body;
+  console.log(req.body);
+  if (!name || !exercises) {
+    res.status(400);
+    throw new Error("All fields are required!");
+  }
+  console.log(exercises);
+  const createdWorkout = await Workout.create({
+    name: name,
+    userId: req.user.id,
+    exercises: exercises,
+  });
+  res.status(200).json({ message: createdWorkout });
+});
 
 //@desc get a workout
 //@route GET /api/workout/
