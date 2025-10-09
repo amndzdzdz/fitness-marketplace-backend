36d7435252f447ac56041f53b77bbf36c5eb045b
amndzdzdz <zorrofx@gmx.net>
2025-10-09 20:57:04 +0200

    added controllers to router

--- routes/workoutSessionRoutes.js
+++ routes/workoutSessionRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const validateToken = require("../middleware/validateTokenHandler");
+const {
+  getWorkoutSessions,
+  getWorkoutSession,
+  createWorkoutSession,
+} = require("../controllers/workoutSessionController");
 const router = express.Router();
 
 router
-  .get("/", validateToken)
-  .put("/", validateToken)
-  .post("/", validateToken)
-  .delete("/", validateToken);
+  .get("/", validateToken, getWorkoutSession)
+  .post("/", validateToken, createWorkoutSession);
+router.get("/all", validateToken, getWorkoutSessions);
 
 module.exports = router;
