3103eb64dbee6aefa2bef72eb708f53a980c775e
amndzdzdz <zorrofx@gmx.net>
2025-10-09 20:54:10 +0200

    added createWorkoutSession controller

--- controllers/workoutSessionController.js
+++ controllers/workoutSessionController.js
@@ -18,4 +18,21 @@ const getWorkoutSessions = asyncHandler(async (req, res) => {
 //@desc Create a workoutSession
 //@route POST /api/workoutSession/
 //@access private
-const createWorkoutSession = "";
+const createWorkoutSession = asyncHandler(async (req, res) => {
+  const { workoutId, sessionInfo } = req.body;
+  if (!workoutId || sessionInfo.length === 0) {
+    res.status(400);
+    throw new Error("All fields are required!");
+  }
+  const userId = req.user.id;
+  const workoutSession = await WorkoutSession.create({
+    userId: userId,
+    workoutId: workoutId,
+    sessionInfo: sessionInfo,
+  });
+  if (!workoutSession) {
+    res.status(400);
+    throw new Error("An error occured creating the workoutSession");
+  }
+  res.status(200).json({ message: workoutSession });
+});
