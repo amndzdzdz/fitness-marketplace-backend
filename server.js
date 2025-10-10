b09a6e9e0e539dbf9a04cc566ab9c44cc1fd85de
amndzdzdz <zorrofx@gmx.net>
2025-10-10 21:08:32 +0200

    refactored

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
-const dotenv = require("dotenv").config();
 
 const port = process.env.PORT;
 const app = express();
+
 connectDB();
 
 app.use(express.json());
